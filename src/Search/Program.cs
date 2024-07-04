8d71f4988d52eade6ebd1e9ff94426339effaa21
Nabi Karampoor <thisisnabi@outlook.com>
2024-07-04 21:18:28 +0330

    Change endpoint

--- src/Search/Program.cs
+++ src/Search/Program.cs
@@ -1,5 +1,8 @@
+using Elastic.Clients.Elasticsearch;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Search;
 using Search.Infrastructure.Extensions;
+using Search.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -14,8 +17,28 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
- 
+
 app.UseHttpsRedirection();
- 
+
+app.MapGet("/", SearchItems);
+
+
 app.Run();
 
+
+static async Task<Results<Ok<IReadOnlyCollection<CatalogItemIndex>>, NotFound>> SearchItems(string qr, ElasticsearchClient elasticsearch)
+{
+    var response = await elasticsearch.SearchAsync<CatalogItemIndex>(s => s
+        .Index(CatalogItemIndex.IndexName)
+        .From(0)
+        .Size(10)
+        .Query(q => 
+             q.Fuzzy(t => t.Field(x => x.Description).Value(qr)))
+    );
+
+    if (response.IsValidResponse)
+        return TypedResults.Ok(response.Documents);
+
+    return TypedResults.NotFound();
+
+}
